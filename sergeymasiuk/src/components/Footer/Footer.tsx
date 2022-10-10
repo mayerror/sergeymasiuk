88914479b80bb4d321d7387cd3ab51a71c9eabf9
Sergey Masiuk <mayerror@gmail.com>
2022-10-10 16:22:36 +0300

    feat: implement footer component

--- sergeymasiuk/src/components/Footer/Footer.tsx
+++ sergeymasiuk/src/components/Footer/Footer.tsx
@@ -1,20 +1,23 @@
 import './Footer.scss';
+import { BsGithub, BsLinkedin } from 'react-icons/bs';
+import { SiGmail } from 'react-icons/si';
 
 function Footer() {
   return (
     <footer className="footer">
       <div className="footer__wrapper">
-        <a href="https://rs.school/">
-          <img
-            alt="logo"
-            className="footer__logo"
-            src="https://raw.githubusercontent.com/MaryAnzh/rslang-assets/61538a7429ec8a4b3eeab95ff3e595de36afad43/assets/svg/rs_school_white.svg"
-          />
-        </a>
-        <a className="link footer__link" href="https://github.com/mayerror">
-          Sergey Masiuk
-        </a>
-        <p className="footer__copyright">&copy; 2022, React</p>
+        <p className="footer__copyright">&copy; 2022, Sergey Masiuk</p>
+        <div className="footer__social-list">
+          <a className="link footer__link" href="https://www.linkedin.com/in/sergey-masiuk/">
+            <BsLinkedin className="social-icon" size={28} />
+          </a>
+          <a className="link footer__link" href="https://github.com/mayerror">
+            <BsGithub className="social-icon" size={28} />
+          </a>
+          <a className="link footer__link" href="mailto:mayerror@gmail.com">
+            <SiGmail className="social-icon" size={28} />
+          </a>
+        </div>
       </div>
     </footer>
   );
