6c1fa995c4a1abd2a355a82a12280595c3c2ec28
Sergey Masiuk <mayerror@gmail.com>
2022-10-24 16:38:19 +0300

    feat: implement about page layout

--- sergeymasiuk/src/pages/About/About.tsx
+++ sergeymasiuk/src/pages/About/About.tsx
@@ -1,7 +1,25 @@
 import './About.scss';
+import logo from '../../assets/images/avatar_result.webp';
 
 function About() {
-  return <div>About</div>;
+  return (
+    <main className="main about">
+      <div>
+        <h1 className="about__title">Hello, my name is Sergey</h1>
+        <p className="about__text">
+          I am a Software Designer / Engineer based in Tempe, AZ. Currently I am working at ui.dev
+          helping folks level up with JavaScript. Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Vero fugit et quaerat, commodi iure, eaque corrupti recusandae ut
+          blanditiis voluptatem eum nulla sunt neque iusto ullam ratione laudantium quam nobis.
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero fugit et quaerat, commodi
+          iure, eaque corrupti recusandae ut blanditiis voluptatem eum nulla sunt neque iusto ullam
+          ratione laudantium quam nobis. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Vero fugit et quaerat, commodi iure
+        </p>
+      </div>
+      <img alt="logo" className="about__image" src={logo} />
+    </main>
+  );
 }
 
 export default About;
--- sergeymasiuk/src/pages/NotFound/NotFound.tsx
+++ sergeymasiuk/src/pages/NotFound/NotFound.tsx
@@ -1,7 +1,12 @@
 import './NotFound.scss';
 
 function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <main className="main not-found">
+      <h1 className="not-found__title">404</h1>
+      <p className="not-found__text">Page not found</p>
+    </main>
+  );
 }
 
 export default NotFound;
