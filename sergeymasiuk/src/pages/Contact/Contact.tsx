5f363c10c07c223e4c4d4a15adf43befab1b4489
Sergey Masiuk <mayerror@gmail.com>
2023-01-17 03:04:12 +0300

    feat: connect react hook form

--- sergeymasiuk/src/pages/Contact/Contact.tsx
+++ sergeymasiuk/src/pages/Contact/Contact.tsx
@@ -1,10 +1,22 @@
 import './Contact.scss';
+import { useForm } from 'react-hook-form';
 
 function Contact() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data: any) => console.log(data);
+
   return (
-    <main className="main not-found">
-      <h1 className="not-found__title">Contact</h1>
-      <form action=""></form>
+    <main className="main contact">
+      <h1 className="contact__title">Contact</h1>
+      <form className="contact__form" onSubmit={handleSubmit(onSubmit)}>
+        <input type="email" {...register('email')} />
+        <input type="submit" value="Submit" />
+      </form>
     </main>
   );
 }
