257f3489f1de215ba630a691416330938cab675b
Sergey Masiuk <mayerror@gmail.com>
2023-01-19 02:32:05 +0300

    feat: add contact form styling

--- sergeymasiuk/src/pages/Contact/Contact.tsx
+++ sergeymasiuk/src/pages/Contact/Contact.tsx
@@ -1,21 +1,46 @@
 import './Contact.scss';
 import { useForm } from 'react-hook-form';
 
+type FormData = {
+  email: string;
+  name: string;
+};
+
 function Contact() {
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm();
-  const onSubmit = (data: any) => console.log(data);
+  } = useForm<FormData>();
+
+  const onSubmit = (data: FormData) => console.log(data);
+  const onError = (errorData: any) => console.log(errorData);
 
   return (
     <main className="main contact">
       <h1 className="contact__title">Contact</h1>
-      <form className="contact__form" onSubmit={handleSubmit(onSubmit)}>
-        <input type="email" {...register('email')} />
-        <input type="submit" value="Submit" />
+      <form className="contact__form" noValidate onSubmit={handleSubmit(onSubmit, onError)}>
+        <input
+          className="contact__input contact__single"
+          type="email"
+          {...register('email', {
+            required: true,
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              message: 'invalid email address',
+            },
+          })}
+        />
+        <input
+          className="contact__input contact__single"
+          type="text"
+          {...register('name', {
+            required: true,
+          })}
+        />
+        <textarea className="contact__input contact__text" />
+        <input className="contact__submit" type="submit" value="Submit" />
       </form>
     </main>
   );
