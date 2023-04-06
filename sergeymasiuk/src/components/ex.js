a17b20e6f6dcd943184e84ffc2eed5392545a92c
Sergey Masiuk <mayerror@gmail.com>
2023-04-06 16:03:40 +0300

    feat: add interactive logo

--- sergeymasiuk/src/components/ex.js
+++ sergeymasiuk/src/components/ex.js
@@ -1,7 +1,110 @@
-function pascalsTriangle(n) {
-  const arr = Array(n).fill(1).map((item, index) => {
+// function pascalsTriangle(n) {
+//   const arr = Array(n)
+//     .fill(1)
+//     .map((_, index) => Array(index + 1).fill(1))
+//     .map((y, index) => {
+//       const item = y;
+//       if (index >= 2) {
+//         item.map((x, i) => {
+//           if (i !== 0 && i !== item.length) {
+//             return item[index-1][i-1] + item[index-1][i];
+//           }
+//           return x;
+//         });
+//       }
+//       return item;
+//     });
+//   return arr;
+// }
 
+// console.log(pascalsTriangle(5));
+
+// function prefill(n, v) {
+//   console.log(n);
+//   // if (!n) return [];
+//   const num = +n;
+//   if (!(typeof n === 'number' || typeof n === 'string') || +n < 0 || Number.isNaN(n))
+//     throw new TypeError(`${n} is invalid!`);
+//   return Array(num).fill(v);
+// }
+
+// // throw Error('example error')
+// console.log(prefill(NaN, 1));
+
+// try {
+//   throw new TypeError("Hello", "someFile.js", 10);
+// } catch (e) {
+//   console.log(e instanceof TypeError); // true
+//   console.log(e.message); // "Hello"
+//   console.log(e.name); // "TypeError"
+//   console.log(e.fileName); // "someFile.js"
+//   console.log(e.lineNumber); // 10
+//   console.log(e.columnNumber); // 0
+//   console.log(e.stack); // "@Scratchpad/2:2:9\n"
+// }
+
+// function sumDigPow(a, b) {
+//   return Array.from(Array(b - a).keys())
+//     .map((x) => x + a)
+//     .reduce((p, c) => {
+//       if (c<10 ||
+//         String(c)
+//           .split('')
+//           .reduce((x, y, i) => +x + y ** (i + 1)) === c
+//       )
+//         return [...p, c];
+//       return p;
+//     }, []);
+// }
+
+// console.log(sumDigPow(1, 10));
+// console.log(sumDigPow(1, 100));
+// console.log(sumDigPow(10, 100));
+
+
+
+// // console.log(checkWord('deerpr'));
+// // console.log(checkWord('deeppyyazr'));
+
+// console.log(solution(['excavate', 'endure', 'desire', 'screen', 'theater', 'excess', 'night']));
+
+function checkWord(word) {
+  return word
+    .slice(1, -1)
+    .split('')
+    .reduce((p, c, i, arr) => {
+      if (!p) return p;
+      if (i % 2 === 0 && c !== arr[i + 1]) return false;
+      return true;
+    }, true);
+}
+
+function enumaration(ar) {
+  const finalArr = [];
+  if (ar.length === 2) {
+    return [`${ar[0]}${ar[1]}`, `${ar[1]}${ar[0]}`];
+  }
+
+  let str = '';
+  for (let i = 0; i < ar.length; i++) {
+    const array = [...ar];
+    array.splice(i,1);
+    const enumArr = enumaration(array);
+    for (let j = 0; j < enumArr.length; j++) {
+      str = ar[i] + enumArr[j];
+      finalArr.push(str);
+    }
+  }
+  return finalArr;
+}
+
+function solution(words) {
+  const arr = words.map((x) => `${x[0]}${x.at(-1)}`);
+  return enumaration(arr).some(word=>{
+    // console.log(word);
+    return checkWord(word);
   });
 }
 
-console.log(pascalsTriangle(4));
+// console.log(enumaration(['1', '2', '3', '4']));
+console.log(solution(["excavate", "endure", "desire", "screen", "theater", "excess", "night"]));
--- sergeymasiuk/src/pages/About/About.tsx
+++ sergeymasiuk/src/pages/About/About.tsx
@@ -1,22 +1,54 @@
 import './About.scss';
 import logo from '../../assets/images/avatar_result.webp';
+import relogo from '../../assets/images/icons8-react-native-48.png';
+import htmllogo from '../../assets/images/icons8-html-5-48.png';
+import csslogo from '../../assets/images/icons8-css3-48.png';
+import jslogo from '../../assets/images/icons8-javascript-48.png';
+import redlogo from '../../assets/images/icons8-redux-48.png';
+import sasslogo from '../../assets/images/icons8-sass-48.png';
 
 function About() {
   return (
     <main className="main about">
       <div>
         <h1 className="about__title">Hello, my name is Sergey</h1>
-        <p className="about__text">
-          I am a Software Designer / Engineer based in Tempe, AZ. Currently I am working at ui.dev
-          helping folks level up with JavaScript. Lorem ipsum dolor sit amet, consectetur
-          adipisicing elit. Vero fugit et quaerat, commodi iure, eaque corrupti recusandae ut
-          blanditiis voluptatem eum nulla sunt neque iusto ullam ratione laudantium quam nobis.
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero fugit et quaerat, commodi
-          iure, eaque corrupti recusandae ut blanditiis voluptatem eum nulla sunt neque iusto ullam
-          ratione laudantium quam nobis. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Vero fugit et quaerat, commodi iure
+        <p className="about__text about__text-about">
+          I am a junior <b>front-end react developer</b>, I have successfully completed RS school
+          courses.
         </p>
-        <p className="about__text">my resume</p>
+        <p className="about__text about__text-about">
+          At the moment I work as an electrical engineer at an aircraft factory and develop
+          automated process control systems. I want to change the scope of work to front-end
+          development, the direction where complex projects go hand in hand with creativity.
+        </p>
+        <div className="about__stack-wrapper">
+          <p className="about__text about__text-bold">Tech Stack &nbsp;&nbsp;|</p>
+          <div className="tooltip">
+            <img alt="html logo" className="about__brand-logo" src={htmllogo} />
+            <span className="tooltiptext">HTML5</span>
+          </div>
+          <div className="tooltip">
+            <img alt="css logo" className="about__brand-logo" src={csslogo} />
+            <span className="tooltiptext">CSS3</span>
+          </div>
+          <div className="tooltip">
+            <img alt="js logo" className="about__brand-logo" src={jslogo} />
+            <span className="tooltiptext">Javascript</span>
+          </div>
+          <div className="tooltip">
+            <img alt="react logo" className="about__brand-logo" src={relogo} />
+            <span className="tooltiptext">React</span>
+          </div>
+          <div className="tooltip">
+            <img alt="redux logo" className="about__brand-logo" src={redlogo} />
+            <span className="tooltiptext">Redux</span>
+          </div>
+          <div className="tooltip">
+            <img alt="sass logo" className="about__brand-logo" src={sasslogo} />
+            <span className="tooltiptext">Sass</span>
+          </div>
+        </div>
+        <p className="about__text about__text-bold">resume pdf</p>
       </div>
       <img alt="logo" className="about__image" src={logo} />
     </main>
